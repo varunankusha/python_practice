057cf1d052c10397284660717165a7deb6ef62cf
varunankusha <123300378+varunankusha@users.noreply.github.com>
2024-02-10 22:15:29 +0530

    Create PythonPractice3.py

---
+++ PythonPractice3.py
@@ -0,0 +1,16 @@
+class MyClass:
+    x=5
+
+    
+    
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+   
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+
+p1 = Person("John", 36)
+print(p1)
